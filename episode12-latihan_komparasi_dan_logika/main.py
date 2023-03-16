6300677559d0b3802725ce937eeb721f9f54eebd
kempup <yusufzulkarnain91@gmail.com>
2023-03-16 21:11:20 +0700

    update

---
+++ episode12-latihan_komparasi_dan_logika/main.py
@@ -0,0 +1,17 @@
+# Episode latihan logika dan komparasi
+
+# Membuat gabungan area rentang dari angka
+
+# ++++++++++3--------10++++++++++
+
+inputUser = float(input("masukkan angka yang bernilai\nkurang dari 3\natau\nlebih besar dari 10\n:"))
+
+
+#++++++++++++3---------
+isKurangDari = (inputUser < 3)
+print (isKurangDari)
+
+#memeriksa lebih dari 10
+#++++++++++++3---------
+isKurangDari = (inputUser > 10)
+print (isKurangDari)
