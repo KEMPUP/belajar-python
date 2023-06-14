24bf599c38788ee1f9428999dbec22bdf207214e
kempup <yusufzulkarnain91@gmail.com>
2023-06-14 11:45:58 +0700

    eps 21 finish

---
+++ episode21-IF_dan_ELSE_Statement/main.py
@@ -0,0 +1,30 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda?")
+
+# program sebelumnya
+# if kondisi: aksi
+# progam selanjutnya
+
+# 1. program if inline
+#if nama=="ucup" : print("Kamu ganteng abieeezzz!!!")
+#print(f"terimakasih {nama}")
+
+# 2. program if indentation
+
+#if nama=="ucup":
+#        print("kamu ganteng abieeeez!")
+#        print("kamu juga keren banget!")
+#print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama=="otong":
+        print("hai otong si keren!")
+else:
+        print("ah kamu bukan otong! kamu gak keren")
+print("akhir dari program")
